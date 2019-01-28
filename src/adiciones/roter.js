393c98f0fafe9b4045b18f1ed681d30f984eb1a1
Alexander Ibarra <ortizalexander2244@gmail.com>
2019-01-27 20:45:59 -0500

    Se configuro el vue-router

---
+++ src/adiciones/roter.js
@@ -0,0 +1,4 @@
+const rutas= [
+
+];
+export default rutas
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
+//Librerias
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+//Archivos Person
+import routes from './adiciones/roter'
+
+//Use
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
